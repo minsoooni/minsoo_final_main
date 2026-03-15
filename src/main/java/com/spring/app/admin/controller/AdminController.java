4f0aee26476f14f3ee8d6cce0f832dd6b0e2e98b
minsoooni <ansjdhsl@naver.com>
2026-03-15 21:41:22 +0900

    (확인용)

--- src/main/java/com/spring/app/admin/controller/AdminController.java
+++ src/main/java/com/spring/app/admin/controller/AdminController.java
@@ -41,7 +41,7 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/admin")
 public class AdminController {
 	//private final 
-	
+	/* 확 */
 	private final AdminCompanyService companyAdminService;
 	private final AdminMemberService memberAdminService;
 	private final AdminJobPostService adminJobPostService;
